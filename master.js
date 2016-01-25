f0fb4a81dd46460b3b43a241e6d5741c8f5536d4
emilyxie <emilyxie@stanford.edu>
2016-01-25 00:13:27 -0800

    init pages

---
+++ master.js
@@ -0,0 +1,22 @@
+function initPage(){
+	$('#check-in-input').focusin(function() {
+		$('.date-row').removeClass('date-container-hidden');
+	});
+	$('#check-in-input').focusout(function() {
+		$('.date-row').addClass('date-container-hidden');
+	});
+
+	$('#check-out-input').focusin(function() {
+		$('.date-row').removeClass('date-container-hidden');
+	});
+	$('#check-out-input').focusout(function() {
+		$('.date-row').addClass('date-container-hidden');
+	});
+
+	$('.more-options-helper').on('click', function(){
+		$(".more-options-helper .glyphicon-triangle-bottom").toggleClass("glyphicon-triangle-top");
+		$(".more-options-menu").toggleClass("more-options-menu-hidden");
+	});
+}
+
+window.onload = initPage();
